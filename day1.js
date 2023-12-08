07ba490084e135181b53875e72635a4fa210bc47
rztprog <rztprog@outlook.fr>
2023-12-08 11:32:16 +0100

    day2

--- day1.js
+++ day1.js
@@ -5,9 +5,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 
-const textSplited = text.split("\n");
-
-textSplited.forEach((line) => {
+text.split("\n").forEach((line) => {
     let numb = line.replaceAll(/[a-z]/gi, "").split("");
     total += Number(numb[0] + numb[numb.length -1]);
 })
